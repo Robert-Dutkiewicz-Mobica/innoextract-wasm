b577ef2bfc08eb77206be5a1758ddfe90b60a341
Michal Ston <michal.ston@mobica.com>
2023-04-28 14:28:16 +0200

    initial ZIP64 implementation (WiP)

--- src/wasm/nonzip/nonzip.cpp
+++ src/wasm/nonzip/nonzip.cpp
@@ -1,4 +1,5 @@
 #include "nonzip.h"
+#include <inttypes.h>
 #include "crc32.h"
 
 #define NONZIP_PLATFORM
@@ -9,29 +10,44 @@
 
 static inline void filetolf(struct nonzip_lf *lf, struct nonzip_file *f) {
     lf->lf_sig   = NONZIP_LF_SIGNATURE;
-    lf->lf_vere  = f->zf_vere;
-    lf->lf_flags = f->zf_flags;
-    lf->lf_comp  = f->zf_comp;
+    lf->lf_vere = NONZIP_VERNEED_DEF;
+    lf->lf_flags = NONZIP_FLAGS_DEF;
+    lf->lf_comp = NONZIP_COMP_DEF;
     lf->lf_modt  = f->zf_modt;
     lf->lf_modd  = f->zf_modd;
+    lf->lf_csize = 0;
+    lf->lf_usize = 0;
     // CRC, csize and usize are ignored in local header
     lf->lf_fnlen = f->zf_fnlen;
-    lf->lf_eflen = f->zf_eflen;
+    lf->lf_eflen = 0;
+    // lf->lf_eflen = sizeof(struct nonzip_z64ef);
+    // Unassigned fields are ignored anyway 
 }
 
 static inline void filetocf(struct nonzip_cf *cf, struct nonzip_file *f) {
     cf->cf_sig    = NONZIP_CF_SIGNATURE;
-    cf->cf_verm   = f->zf_verm;
-    cf->cf_vere   = f->zf_vere;
-    cf->cf_flags  = f->zf_flags;
-    cf->cf_comp   = f->zf_comp;
+    cf->cf_verm   = NONZIP_VERMADE_DEF;
+    cf->cf_vere   = NONZIP_VERNEED_DEF;
+    cf->cf_flags  = NONZIP_FLAGS_DEF;
+    cf->cf_comp   = NONZIP_COMP_DEF;
     cf->cf_modt   = f->zf_modt;
     cf->cf_modd   = f->zf_modd;
     cf->cf_crc    = f->zf_crc;
-    cf->cf_csize  = f->zf_csize;
-    cf->cf_usize  = f->zf_usize;
+    cf->cf_csize  = IFFITS32(f->zf_csize);
+    cf->cf_usize  = IFFITS32(f->zf_usize);
     cf->cf_fnlen  = f->zf_fnlen;
-    cf->cf_offset = f->zf_offset;
+    cf->cf_dns    = 0;
+    cf->cf_offset = IFFITS32(f->zf_offset);
+    cf->cf_eflen  = sizeof(struct nonzip_z64ef);
+    // Unassigned fields are ignored anyway 
+}
+
+static inline void filetoz64ef(struct nonzip_z64ef *ef, struct nonzip_file *f) {
+    ef->ef_sig    = NONZIP_EF_ZIP64_SIG;
+    ef->ef_size   = sizeof(struct nonzip_z64ef) - 4;
+    ef->ef_csize  = f->zf_csize;
+    ef->ef_usize  = f->zf_usize;
+    ef->ef_offset = f->zf_offset;
 }
 
 static inline struct dostime timetodos(time_t tt) {
@@ -41,27 +57,38 @@ static inline struct dostime timetodos(time_t tt) {
 
 Nonzip::Nonzip(const char *path) {
     emjs::open(path, "wb");
-    dt = timetodos(time(0));
+    dost = timetodos(time(0));
     status = NONZIP_STATUS_READY;
 }
 
-int Nonzip::addFile(const char *name, const void *data, uint32_t dlen, uint32_t *zipindex) {
+void Nonzip::writeDD()
+{
+    if(numfiles > 0){
+        struct nonzip_dd dd;
+        dd.dd_sig = NONZIP_DATA_DESC_SIG;
+        dd.dd_crc = files[numfiles-1]->zf_crc;
+        dd.dd_csize = files[numfiles-1]->zf_csize;
+        dd.dd_usize = files[numfiles-1]->zf_usize;
+        offset += emjs::write(&dd, sizeof(dd), 1);
+    }
+}
+
+int Nonzip::addFile(const char *name, const void *data, size_t dlen, uint32_t *zipindex) {
     if (status != NONZIP_STATUS_READY) return NONZIP_ERR_NOTREADY;
+
+    writeDD(); // Write last file's DD
 
     // Alloc space for lfhs
     if (numfalloc < numfiles + 1) {
         numfalloc = numfalloc ? numfalloc * 3 / 2 : 8;
         files = (struct nonzip_file **) realloc(files, numfalloc * sizeof(struct nonzip_file *));
     }
+
     int i = (numfiles++);
     int nlen = strlen(name);
-    struct dostime dt = dt;
+    struct dostime dt = dost;
     files[i] = (struct nonzip_file *)calloc(1, sizeof(struct nonzip_file));
 
-    files[i]->zf_verm = NONZIP_VERMADE_DEF;
-    files[i]->zf_vere = NONZIP_VERNEED_DEF;
-    files[i]->zf_flags = NONZIP_FLAGS_DEF;
-    files[i]->zf_comp = NONZIP_COMP_DEF;
     files[i]->zf_crc = crc32(~0, (uint8_t*) data, dlen);
     files[i]->zf_csize = dlen;
     files[i]->zf_usize = dlen;
@@ -72,20 +99,27 @@ int Nonzip::addFile(const char *name, const void *data, uint32_t dlen, uint32_t 
     memcpy(files[i]->zf_name, name, nlen);
 
     struct nonzip_lf lf = {0, };
+    // struct nonzip_z64ef ef = {0, };
+    // ef.ef_sig = NONZIP_EF_ZIP64_SIG;
+    // ef.ef_size = sizeof(ef) - 4;
+
     filetolf(&lf, files[i]);
     emjs::write(&lf, sizeof(lf), 1);   // Write header
     emjs::write(name, 1, nlen);        // Write filename
+    // emjs::write(&ef, sizeof(ef), 1);  // write ZIP64 extra field
     dlen = emjs::write(data, 1, dlen); // Write file data
-    offset += sizeof(lf) + nlen + dlen;
 
-    printf("writing file %s: len=%u, offset=%u\n", name, dlen, files[i]->zf_offset);
+    offset += sizeof(lf) + nlen + dlen;
+    // offset += sizeof(lf) + nlen + sizeof(ef) + dlen;
+
+    printf("writing file %s: len=%" "lu" ", offset=%" PRIu64 "\n", name, dlen, files[i]->zf_offset);
     if(zipindex != NULL)
         *zipindex = i;
 
     return dlen;
 }
 
-int Nonzip::appendFile(const void *data, uint32_t dlen) {
+int Nonzip::appendFile(const void *data, size_t dlen) {
     if (status != NONZIP_STATUS_READY)
         return NONZIP_ERR_NOTREADY;
 
@@ -109,28 +143,51 @@ int Nonzip::appendFile(const void *data, uint32_t dlen) {
 int Nonzip::close() {
     if (status != NONZIP_STATUS_READY) return NONZIP_ERR_NOTREADY;
 
+    writeDD(); // Write last file's DD
+
     int i;
     struct nonzip_cf cf = {0, };
-    uint16_t cdsize = 0;
-    uint32_t cdoffset = offset;
+    struct nonzip_z64ef ef = {0, };
+    uint64_t cdsize = 0;
+    uint64_t cdoffset = offset;
     for (i = 0; i < numfiles; i++) {
         filetocf(&cf, files[i]);
+        filetoz64ef(&ef, files[i]);
         cdsize += emjs::write(&cf, sizeof(cf), 1);  // write central file header
         cdsize += emjs::write(files[i]->zf_name, 1, files[i]->zf_fnlen);  // write filename
+        cdsize += emjs::write(&ef, sizeof(ef), 1);  // write ZIP64 extra field
         free(files[i]->zf_name);
         free(files[i]);
     }
     free(files);
 
+    struct nonzip_z64end en64 = {0, };
+    en64.en_sig    = NONZIP_ZIP64_END_SIG;
+    en64.en_size   = sizeof(struct nonzip_z64end) - 12; // struct size - sig/size fields
+    en64.en_verm   = NONZIP_VERMADE_DEF;
+    en64.en_vere   = NONZIP_VERNEED_DEF;
+    en64.en_ennum = numfiles;
+    en64.en_cdennum = numfiles;
+    en64.en_cdoff = cdoffset;
+    en64.en_cdsize = cdsize;
+    emjs::write(&en64, sizeof(en64), 1);
+    
+    struct nonzip_z64loc loc = {0, };
+    loc.loc_sig = NONZIP_ZIP64_LOC_SIG;
+    loc.loc_off = cdoffset + cdsize;
+    emjs::write(&loc, sizeof(loc), 1);
+
     struct nonzip_end end = {0, };
-    memcpy(end.en_comment, nonzip_comment, sizeof(nonzip_comment) - 1);
+    // memset(&end, 0xFF, sizeof(end));
     end.en_sig = NONZIP_END_SIGNATURE;
-    end.en_ennum = numfiles;
-    end.en_cdennum = numfiles;
-    end.en_cdoff = cdoffset;
-    end.en_cdsize = cdsize;
+    end.en_ennum = IFFITS16(numfiles);
+    end.en_cdennum = IFFITS16(numfiles);
+    end.en_cdsize = IFFITS32(cdsize);
+    end.en_cdoff = ~0; // IFFITS32(cdoffset);
     end.en_clen = sizeof(nonzip_comment) - 1;
+    memcpy(end.en_comment, nonzip_comment, sizeof(nonzip_comment) - 1);
     emjs::write(&end, sizeof(end), 1);
+
     emjs::write(NULL, 0, 0);
     emjs::close();
 
@@ -142,8 +199,8 @@ void Nonzip::setTime(uint32_t index, time_t t)
 {
     if(index < numfiles){
         struct dostime dt = timetodos(t);
-        files[index]->zf_modd = dt.date;
-        files[index]->zf_modt = dt.time;
+        files[index]->zf_modd = dost.date;
+        files[index]->zf_modt = dost.time;
     }
     else {
         printf("trying to set time for non-existent file, index=%u, numfiles=%llu\n", index, numfiles);
--- src/wasm/nonzip/nonzip.h
+++ src/wasm/nonzip/nonzip.h
@@ -7,14 +7,18 @@
 #include <stdio.h>
 #include <time.h>
 
-#define NONZIP_LF_SIGNATURE   (0x04034b50)
-#define NONZIP_CF_SIGNATURE   (0x02014b50)
-#define NONZIP_END_SIGNATURE  (0x06054b50)
+#define NONZIP_LF_SIGNATURE   (0x04034b50U)
+#define NONZIP_CF_SIGNATURE   (0x02014b50U)
+#define NONZIP_END_SIGNATURE  (0x06054b50U)
+#define NONZIP_EF_ZIP64_SIG   (0x0001U)
+#define NONZIP_ZIP64_END_SIG  (0x06064b50U)
+#define NONZIP_ZIP64_LOC_SIG  (0x07064b50U)
+#define NONZIP_DATA_DESC_SIG  (0x08074b50U)
 #define NONZIP_VERMADE_DEF    (63U) // Version 6.3 of document
-#define NONZIP_VERNEED_DEF    (41U) // Version 4.1 required for UTF
+#define NONZIP_VERNEED_DEF    (45U) // Version 4.5 required for ZIP64
 
-// Enable UTF filenames and comments, lengths and CRC only correct in central header
-#define NONZIP_FLAGS_DEF      ((1<<11) | (1<<3))
+// Bit 11: Enable UTF filenames and comments, bit 3: lengths and CRC only correct in central header
+#define NONZIP_FLAGS_DEF      (/*(1<<11) | */(1<<3))
 #define NONZIP_COMP_DEF       (0U) // No compression
 #define NONZIP_DISK_DEF       (0U) // only "disk 0" used
 #define NONZIP_ATTRS_DEF      (0U) // attributes might be set to 0
@@ -24,6 +28,9 @@
 #define NONZIP_ERR_NOTREADY   (-1)
 const char nonzip_comment[] = "Created with nonzip by Michał Stoń <michal.ston@mobica.com>";
 
+#define IFFITS32(x) (x < 0xFFFFFFFF ? x : 0xFFFFFFFF) // return var if fits in 32b, otherwise 0xFFFFFFFF
+#define IFFITS16(x) (x < 0xFFFF ? x : 0xFFFF) // return var if fits in 32b, otherwise 0xFFFFFFFF
+
 struct dostime{
     uint16_t time;
     uint16_t date;
@@ -31,22 +38,18 @@ struct dostime{
 
 // Struct for internal use to keep all fire parameters used by CFH and LFH
 struct nonzip_file {
-    uint16_t zf_verm;   // version made by
-    uint16_t zf_vere;   // version needed to extract
-    uint16_t zf_flags;  // general purpose bit flag
-    uint16_t zf_comp;   // compression method
     uint16_t zf_modt;   // last mod file time
     uint16_t zf_modd;   // last mod file date
     uint32_t zf_crc;    // crc-32
-    uint32_t zf_csize;  // compressed size
-    uint32_t zf_usize;  // uncompressed size
+    uint64_t zf_csize;  // compressed size
+    uint64_t zf_usize;  // uncompressed size
     uint16_t zf_fnlen;  // file name length
-    uint16_t zf_eflen;  // extra field length
-    uint16_t zf_fclen;  // file comment length
+    // uint16_t zf_eflen;  // extra field length
+    // uint16_t zf_fclen;  // file comment length
     uint16_t zf_dns;    // disk number start
-    uint16_t zf_attri;  // internal file attributes
-    uint32_t zf_attre;  // external file attributes
-    uint32_t zf_offset;  // relative offset of local header
+    // uint16_t zf_attri;  // internal file attributes
+    // uint32_t zf_attre;  // external file attributes
+    uint64_t zf_offset;  // relative offset of local header
     char     *zf_name;
     char     *zf_data;
 };
@@ -111,41 +114,66 @@ struct nonzip_end {
     char en_comment[sizeof(nonzip_comment)-1]; // comment
 };
 
+// ZIP64 
+struct nonzip_z64ef {
+    uint16_t ef_sig;    // extra field signature
+    uint16_t ef_size;   // extra field size
+    uint64_t ef_usize;  // uncompressed size
+    uint64_t ef_csize;  // compressed size
+    uint64_t ef_offset; // relative file header offset
+};
+
+struct nonzip_z64end {
+    uint32_t en_sig;    // end of central dir signature
+    uint64_t en_size;   // size of this directory // size of this struct - 12(sig+size)
+    uint16_t en_verm;   // version made by
+    uint16_t en_vere;   // version needed to extract
+    uint32_t en_dnum;   // number of this disk
+    uint32_t en_cdnum;  // number of the disk with the
+                        // start of the central directory
+    uint64_t en_ennum;  // total number of entries in the
+                        // central directory on this disk
+    uint64_t en_cdennum;// total number of entries in
+                        // the central directory
+    uint64_t en_cdsize; // size of the central directory
+    uint64_t en_cdoff;  // offset of start of central
+                        // directory with respect to
+                        // the starting disk number
+};
+
+struct nonzip_z64loc {
+    uint32_t loc_sig;   // extra field signature
+    uint32_t loc_endnum;// number of the disk with the Z64 CD end record
+    uint64_t loc_off;   // offset of start of Z64 CD end record
+    uint32_t loc_numd;  // total number of disks    
+};
+
+struct nonzip_dd {
+    uint32_t dd_sig;    // extra field signature
+    uint32_t dd_crc;    // crc32
+    uint64_t dd_usize;  // uncompressed size
+    uint64_t dd_csize;  // compressed size
+};
+
+
 #pragma pack(pop)
-typedef struct nonzip_zip {
-    int status;
-    uint64_t numfiles;
-    uint64_t numfalloc;
-    uint32_t offset;
-    struct nonzip_end *end;
-    struct nonzip_file **files;
-    struct dostime dt;
-} nonzip_t;
-
-
-
 
 class Nonzip {
 private:
     int status;
     uint64_t numfiles;
     uint64_t numfalloc;
-    uint32_t offset;
+    uint64_t offset;
     struct nonzip_file **files;
-    struct dostime dt;
+    struct dostime dost;
+    void writeDD();
 public:
     Nonzip(const char *path);
-    int addFile(const char *name, const void *data, uint32_t dlen, uint32_t *zipindex);
-    int appendFile(const void *data, uint32_t dlen);
+    int addFile(const char *name, const void *data, size_t dlen, uint32_t *zipindex);
+    int appendFile(const void *data, size_t dlen);
     int close();
     int getStatus();
     void setTime(uint32_t index, time_t t);
 };
-
-// nonzip_t nonzip_open(const char *path);
-// int nonzip_addfile(nonzip_t *z, const char *name, const void *data, uint32_t dlen, uint32_t *zipindex);
-// int nonzip_appendfile(nonzip_t *z, const void *data, uint32_t dlen);
-// void nonzip_settime(nonzip_t *z, uint32_t index, time_t t);
-// int nonzip_close(nonzip_t *z);
 
 #endif /* NONZIP_H */
